8c5f6701b305bf2b8491201af653bf3939926b90
sazad44 <530@holbertonschool.com>
2019-05-18 06:00:37 +0000

    Updating css style to contain amenity list, and updating javascript script to put amenity names in h4 tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,13 +2,16 @@ $(document).ready(() => {
   let amenityDict = {};
   $('input').change(() => {
     if ($('input').prop('checked')) {
-      $('input[type=checkbox]:checked').each((amenity) => {
-        alert($(this).attr('data-name'));
-        amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
-      });
+      $("li > input:checkbox:checked").map(function(){
+	amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+      }).get();
       $('div.amenities > h4').text(Object.values(amenityDict));
     } else {
-      amenityDict[$('input').attr('data-id')] = null;
+      amenityDict = {};
+      $("li > input:checkbox:checked").map(function(){
+	amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+      }).get();
+      $('div.amenities > h4').text(Object.values(amenityDict));
     }
   });
 });
