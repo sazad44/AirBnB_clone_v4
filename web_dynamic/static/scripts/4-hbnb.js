65e497a83e7716c441d8fa2b9d8864301ddede39
sazad44 <530@holbertonschool.com>
2019-05-20 20:02:36 +0000

    Updating styling of webpage and cleaning up request format in js files

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,33 +1,41 @@
 $(document).ready(() => {
   let amenityDict = {};
   $('input:checkbox').change(() => {
+    let amenityString = '';
     if ($(this).is(':checked')) {
       $('li > input:checkbox:checked').map(function () {
-        amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+        amenityDict[$(this).attr('data-id')] = ' ' + $(this).attr('data-name');
       }).get();
-      $('div.amenities > h4').text(Object.values(amenityDict));
+      Object.values(amenityDict).forEach((amenity) => {
+        amenityString += amenity;
+        amenityString += ' ';
+      });
+      $('div.amenities > h4').text(amenityString);
     } else {
       amenityDict = {};
       $('li > input:checkbox:checked').map(function () {
         amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
       }).get();
-      $('div.amenities > h4').text(Object.values(amenityDict));
+      Object.values(amenityDict).forEach((amenity, i) => {
+        amenityString += amenity;
+        if (i !== Object.values(amenityDict).length - 1) {
+          amenityString += ', ';
+        }
+      });
+      $('div.amenities > h4').text(amenityString);
     }
   });
 
-  let request = new XMLHttpRequest();
-  const url = 'http://0.0.0.0:5001/api/v1/status/';
-
-  request.responseType = 'json';
-  request.open('GET', url, true);
-  request.onreadystatechange = function () {
-    if (request.response['status'] === 'OK') {
-      $('div#api_status').addClass('available');
-    } else {
-      $('div#api_status').removeClass('available');
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: (status) => {
+      if (status.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
     }
-  };
-  request.send();
+  });
 
   let userNames = [];
 
@@ -78,7 +86,7 @@ $(document).ready(() => {
     });
   };
 
-placeFilter(Object.keys(amenityDict));
+  placeFilter(Object.keys(amenityDict));
 
   $('button').click(() => {
     placeFilter(Object.keys(amenityDict));
